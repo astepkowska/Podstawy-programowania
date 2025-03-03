cfb4ac06ada81f20fa1041bae97d80c6b370578a
astepkowska <a.stepkowska.664@studms.ug.edu.pl>
2025-03-03 16:36:00 +0100

    koniec zajęć

---
+++ Zadanie 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, h, V, Pc, l;
+            Console.WriteLine("Podaj promień r=");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj wysokość h=");
+            h = Convert.ToDouble(Console.ReadLine());\
+            l = Math.Sqrt(h * h+ r * r); //tworząca stożka
+            V=1/3.0
+        }
+    }
+}
