a8df5ff9f80329d07ac3d861992f554848e8d372
astepkowska <a.stepkowska.664@studms.ug.edu.pl>
2025-03-03 16:22:23 +0100

    koniec zadania 4

---
+++ Zadanie 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Podaj a=");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj b=");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Pole={0}", a * b);
+            Console.WriteLine("Obwód={0}", 2 * a + 2 * b);
+            Console.ReadKey(true); //pauza
+        }
+    }
+}
