9023f34d49ccf300b0852affa5568898e3ea5c84
astepkowska <a.stepkowska.664@studms.ug.edu.pl>
2025-03-03 16:14:38 +0100

    koniec zadania 3

---
+++ Zadanie 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj a= ");
+            a= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a^3={0}", a * a * a);
+            Console.ReadKey(true); //pauza
+        }
+    }
+}
