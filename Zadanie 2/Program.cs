0decfc551e22521ad11b5730a34231340095d8af
astepkowska <a.stepkowska.664@studms.ug.edu.pl>
2025-03-03 16:07:11 +0100

    koniec zadania 2

---
+++ Zadanie 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie;
+            Console.Write("Podaj swoje imie ");
+            imie = Console.ReadLine();
+            Console.WriteLine("Witaj {0}!", imie);
+            Console.ReadKey(true); //pauza
+        }
+    }
+}
