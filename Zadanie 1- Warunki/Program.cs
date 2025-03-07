12fd9d1c2bb2bd58ee6aa3f6307928911bce8094
astepkowska <a.stepkowska.664@studms.ug.edu.pl>
2025-03-07 14:29:16 +0100

    koniec zadania 2- warunki

---
+++ Zadanie 1- Warunki/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zadanie_1__Warunki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj liczbę całkowitą ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+                Console.WriteLine("Liczba jest parzysta");
+            else
+                Console.WriteLine("Liczba jest nieparzysta");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zadanie 2-Warunki/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadanie_2_Warunki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj liczbę całkowitą ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0)
+                Console.WriteLine("Liczba {0} jest dodatnia.", a);
+            else if (a < 0)
+                Console.WriteLine("Liczba {0} jest ujemna.", a);
+            else
+                Console.WriteLine("Liczba {0} jest równa 0.", a);
+            Console.ReadKey(true);
+
+        }
+    }
+}
