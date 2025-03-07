c4ce4645689af9fba7b294e7e06acd8cc9f21a43
astepkowska <a.stepkowska.664@studms.ug.edu.pl>
2025-03-07 14:11:05 +0100

    Koniec zadania 7

---
+++ Zadanie 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zadanie_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, suma, różnica, iloraz, iloczyn;
+            Console.Write("Podaj liczbę a= ");
+            a= Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj liczbę b= ");
+            b= Convert.ToDouble(Console.ReadLine());
+            suma = a + b;
+            różnica = a - b;
+            iloczyn = a * b;
+            iloraz = a / b;
+            Console.WriteLine("Suma wynosi: {0}", suma);
+            Console.WriteLine("Różnica wynosi: {0}", różnica); 
+            Console.WriteLine("Iloczyn wynosi: {0}", iloczyn);
+            Console.WriteLine("Iloraz wynosi: {0}", iloraz);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zadanie 7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zadanie_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Podaj liczbę a=");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj liczbę b=");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}/{1}={2}r{3}", a, b, a / b, a % b);
+            Console.ReadKey(true);
+        }
+    }
+}
