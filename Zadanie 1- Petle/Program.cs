d89857367879c759738a7edb2015a556a217bbc0
Aleksandra Stępkowska <a.stepkowska.664@studms.ug.edu.pl>
2025-03-17 16:14:59 +0100

    Zakończenie zadań na dziś 17.03

---
+++ Zadanie 1- Petle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Napisać program wypisujący na ekranie kolejne liczby całkowite od 11 do 33. Użyj pętli for.
+            int i;
+            for (int i = 11; i <= 33; i++)
+            {
+                Console.WriteLine("{0}, ",i);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zadanie 3-Petle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący liczbę całkowitą n (n>2) i wypisujący na ekranie wartość sumy
+            //1 + 2 +…+n.Do obliczenia wartości sumy użyj pętli for.
+            int i, suma = 0, n;
+            Console.WriteLine("Podaj liczbę całkowitą n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                suma += i;
+            }
+            Console.WriteLine("Suma 1+2+...+{0} = {1}", n, suma);
+            Console.ReadKey(true);
+        }
+    }
+}
